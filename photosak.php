113bef998c2145e3b427ef31d6502cd300179766
acer e5-774g <atileckis@gmail.com>
2025-11-24 10:26:43 +0200

    (pārbaude vai git Hub strādā)

--- photosak.php
+++ photosak.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" lang="lv">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>photo</title>
+    <link rel="stylesheet" href="photosak.css">
 </head>
 <body>
     
